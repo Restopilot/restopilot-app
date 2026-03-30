e0ae832d521fab42ba8228a64204029ff816ad8b
Restopilot <a.expertdiag@gmail.com>
2026-03-30 20:16:41 +0200

    Update send-report.js

--- api/send-report.js
+++ api/send-report.js
@@ -185,7 +185,7 @@ function buildEmailHTML(date, ca, monthlyRatio) {
 </body></html>`;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
