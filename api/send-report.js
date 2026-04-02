3c305ee1411663ff6a58b86a5f753fcfca7d9fb2
Restopilot <a.expertdiag@gmail.com>
2026-04-02 22:21:20 +0200

    Update send-report.js

--- api/send-report.js
+++ api/send-report.js
@@ -67,7 +67,7 @@ async function getZeltyCA(date) {
 }
 
 async function getRecipients() {
-  const resp = await fetch(`${SUPABASE_URL}/rest/v1/alert_recipients?active=eq.true&select=email,name`, {
+  const resp = await fetch(`${SUPABASE_URL}/rest/v1/alert_recipients?active=eq.true&restaurant_id=eq.${AFRIK_FUSION_ID}&select=email,name`, {
     headers: { apikey: SUPABASE_KEY, Authorization: `Bearer ${SUPABASE_KEY}` },
   });
   return resp.json();
